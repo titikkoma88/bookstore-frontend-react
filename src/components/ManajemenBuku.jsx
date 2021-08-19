9bc154f961873c2797bb7dd3d075a4a4652115e9
titikkoma88 <jie.koma88@gmail.com>
2021-08-19 11:10:26 +0700

    Create component ManajemenBuku

---
+++ src/components/ManajemenBuku.jsx
@@ -0,0 +1,16 @@
+function ManajemenBuku() {
+    // data
+    // event handling
+    return (
+        <div className="container mt-3">
+            <h1 className="text-center">Manajemen Buku</h1>
+
+            <div id="formTambah"></div>
+            <div id="formUbah"></div>
+            <div id="daftarBuku"></div>
+
+        </div>
+    );
+}
+
+export default ManajemenBuku;
