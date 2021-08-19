579b0511764ddbb511b7a419a9c2086ec0556f95
titikkoma88 <jie.koma88@gmail.com>
2021-08-19 14:14:55 +0700

    Display data katalog buku on the homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 
 
 function App() {
-  const [books] = useState([
+  const [books, setBooks] = useState([
     {_id: 1, judul: "Bumi", pengarang: "Tere Liya", harga: 80000, stok: 7},
     {_id: 2, judul: "New York Hari Ini", pengarang: "Aan Mansyur", harga: 85000, stok: 5},
   ]);
@@ -30,7 +30,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact>
-            <Beranda />
+            <Beranda bookList={books} />
           </Route>
 
           <Route path="/manajemen-buku">
--- src/components/Beranda.jsx
+++ src/components/Beranda.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React from "react";
 
-function Beranda() {
-    return (
-        <div className="container mt-3 w-75">
-            <h1 className="text-center">Selamat datang di toko buku Camp404!</h1>
+function Beranda({ bookList }) {
+  console.log(bookList);
 
-            <div id="katalog"></div>
-        </div>
-    );
+  return (
+    <div className="container mt-3 w-75">
+      <h1 className="text-center">Selamat datang di toko buku Camp404!</h1>
+
+      <div id="katalogBuku" className="mt-5">
+        <h2>Katalog Buku</h2>
+        <hr />
+        <table className="table table-hover table-bordered">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Judul</th>
+              <th>Pengarang</th>
+              <th>Harga</th>
+              <th>Stok</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookList.map((book, index) => (
+              <tr key={book._id}>
+                <td>{index + 1}</td>
+                <td>{book.judul}</td>
+                <td>{book.pengarang}</td>
+                <td>{book.harga}</td>
+                <td>{book.stok}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Beranda;
