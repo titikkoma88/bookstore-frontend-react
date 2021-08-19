a58bd91746eba9bb803d51ca379349fbfecf7ff6
titikkoma88 <jie.koma88@gmail.com>
2021-08-19 11:19:35 +0700

    Create component beranda

---
+++ src/components/Beranda.jsx
@@ -0,0 +1,11 @@
+function Beranda() {
+    return (
+        <div className="container mt-3 w-75">
+            <h1 className="text-center">Selamat datang di toko buku Camp404!</h1>
+
+            <div id="katalog"></div>
+        </div>
+    );
+}
+
+export default Beranda;
