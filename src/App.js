ccf78d240b70f75d7362a6a73ef5e6017eb4565e
titikkoma88 <jie.koma88@gmail.com>
2021-08-19 13:09:01 +0700

    Add dummy data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Beranda from "./components/Beranda";
 import Navbar from "./components/Navbar";
 import ManajemenBuku from "./components/ManajemenBuku";
+import { useState } from 'react';
+
 
 function App() {
+  const [books,setBooks] = useState([
+    {_id: 1, judul: "Bumi", pengarang: "Tere Liya", harga: 80000, stok: 7},
+    {_id: 2, judul: "New York Hari Ini", pengarang: "Aan Mansyur", harga: 85000, stok: 5},
+  ]);
   return (
     <div>
-      <BrowserRouter>
+      <Router>
         <Navbar />
         <Switch>
           <Route path="/" exact>
@@ -14,10 +21,10 @@ function App() {
           </Route>
 
           <Route path="/manajemen-buku">
-            <ManajemenBuku />
+            <ManajemenBuku bookList={books} />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
--- src/components/ManajemenBuku.jsx
+++ src/components/ManajemenBuku.jsx
@@ -1,18 +1,69 @@
-import React from 'react';
+import React from "react";
 
-function ManajemenBuku() {
-    // data
-    // event handling
-    return (
-        <div className="container mt-3">
-            <h1 className="text-center">Manajemen Buku</h1>
+function ManajemenBuku({ bookList }) {
+  // data
+  console.log(bookList);
+  // event handling
+  return (
+    <div className="container mt-3">
+      <h1 className="text-center">Manajemen Buku</h1>
 
-            <div id="formTambah"></div>
-            <div id="formUbah"></div>
-            <div id="daftarBuku"></div>
-
-        </div>
-    );
+      <div id="formTambah">
+      <h5>Tambah Buku</h5>
+            <hr />
+            <form className="form row">
+                <div className="col-3">
+                    <input type="text" name="judul" className="form-control mx-2" placeholder="Judul" />
+                </div>
+                <div className="col-3">
+                    <input type="text" name="pengarang" className="form-control mx-2" placeholder="Pengarang" />
+                </div>
+                <div className="col-2">
+                    <input type="text" name="harga" className="form-control mx-2" placeholder="Harga" />
+                </div>
+                <div className="col-2">
+                    <input type="number" name="stok" className="form-control mx-2" placeholder="Stok"  />
+                </div>
+                <div className="col-2">
+                    <input type="submit" className="btn btn-primary ms-5" value="simpan" />
+                </div>
+            </form>
+      </div>
+      <div id="formUbah"></div>
+      <div id="daftarBuku">
+        <h2 className="mt-3">Daftar Buku</h2>
+        <hr />
+        <button className="btn btn-primary m-2">Tambah Buku</button>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Judul</th>
+              <th>Pengarang</th>
+              <th>Harga</th>
+              <th>Stok</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookList.map((book, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{book.judul}</td>
+                <td>{book.pengarang}</td>
+                <td>{book.harga}</td>
+                <td>{book.stok}</td>
+                <td>
+                  <button className="btn btn-info me-3">Edit</button>
+                  <button className="btn btn-danger">Hapus</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default ManajemenBuku;
